428f08c042877e50623188cce709543abe8bd003
Patrick Schimmel <patrick.schimmel@gs-cms.com>
2023-01-28 21:04:15 +0100

    Added generic IView interface

---
+++ ES.Tools.Core/Infrastructure/EventService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ES.Tools.Core.Infrastructure
+{
+  public class EventService
+  {
+    private static readonly Lazy<EventService> _service = new(()=> new EventService());
+    private readonly Dictionary<string, IList> _events;
+
+    private EventService()
+    {
+      _events = new Dictionary<string, IList>();
+    }
+
+    public static EventService Instance => _service.Value;
+
+    public void Subscribe<T>(Type type, Action<T> action)
+    {
+      Subscribe(type?.FullName ?? throw new ArgumentNullException(nameof(type), "Type name cannot be empty."), action);
+    }
+
+    public void Subscribe<T>(string name, Action<T> action)
+    {
+      if (action == null)
+      {
+        throw new ArgumentNullException(nameof(action));
+      }
+
+      if (!_events.ContainsKey(name))
+      {
+        _events[name] = new List<Action<T>>();
+      }
+
+      _events[name].Add(action);
+    }
+
+    public void Unsubscribe(string name)
+    {
+      _events.Remove(name);
+    }
+
+    public void Publish<T>(Type type, T payload)
+    {
+      Publish(type?.FullName ?? throw new ArgumentNullException(nameof(type), "Type name cannot be empty."), payload);
+    }
+
+    public void Publish<T>(string name, T payload)
+    {
+      if (_events.TryGetValue(name, out IList delegateList))
+      {
+        if (delegateList == null)
+        {
+          throw new ArgumentOutOfRangeException($"The named event '{name}' was not registered.");
+        }
+
+        foreach (var delegateItem in delegateList)
+        {
+          ((Action<T>)delegateItem).Invoke(payload);
+        }
+      }
+      else
+      {
+        throw new ArgumentOutOfRangeException($"The named event '{name}' was not registered.");
+      }
+    }
+  }
+}
---
+++ ES.Tools.Core/MVVM/IViewOfT.cs
@@ -0,0 +1,7 @@
+﻿namespace ES.Tools.Core.MVVM
+{
+  public interface IView<T> : IView where T : class, IViewModel
+  {
+    new T ViewModel { get; set; }
+  }
+}
